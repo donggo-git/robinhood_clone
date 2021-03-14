50bedad7f7f5243a96e0926b939fe951f7e4a01a
Dong <Dlttthvlhhth@gmail.com>
2021-03-13 21:35:33 -0800

    add style for timeline

--- src/TimeLine.js
+++ src/TimeLine.js
@@ -1,15 +1,17 @@
 import React from 'react'
-
+import './TimeLine.css'
 function TimeLine() {
     return (
         <div>
             <div className='timeline__container'>
-                <div className='timeline__button'>LIVE</div>
-                <div className='timeline__button'>1D</div>
-                <div className='timeline__button active'>1W</div>
-                <div className='timeline__button'>3M</div>
-                <div className='timeline__button'>1Y</div>
-                <div className='timeline__button'>ALL</div>
+                <div className='timeline__buttons__container'>
+                    <div className='timeline__button'>LIVE</div>
+                    <div className='timeline__button'>1D</div>
+                    <div className='timeline__button active'>1W</div>
+                    <div className='timeline__button'>3M</div>
+                    <div className='timeline__button'>1Y</div>
+                    <div className='timeline__button'>ALL</div>
+                </div>
             </div>
         </div>
     )
