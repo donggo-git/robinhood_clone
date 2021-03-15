7014f28005916458bd01af72b3f785b78585e7eb
Dong <Dlttthvlhhth@gmail.com>
2021-03-14 23:37:01 -0700

    newsfeed buying section

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -16,6 +16,10 @@ function Newsfeed() {
                         <TimeLine />
                     </div>
                 </div>
+                <div className='newsfeed__buying__section'>
+                    <h2>Buying Power</h2>
+                    <h2>$4.11</h2>
+                </div>
             </div>
         </div>
     )
